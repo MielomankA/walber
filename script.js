5b0387720175d14e8855bb776a59ba2be71a2cdd
MielomankA <mielomanka@mail.ru>
2021-11-30 01:55:27 +0300

    fix: close burger, change styles, block card-links

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const header = document.querySelector('.header');
-const games = document.getElementById('games');
-const programms = document.getElementById('programms');
+// const games = document.getElementById('games');
+// const programms = document.getElementById('programms');
 const download = document.getElementById('download');
+const downloadMain = document.querySelector('.download-main');
+const downloadError = document.querySelector('.download-error');
 const linkGames = document.querySelector('.link-games');
 const linkProgramms = document.querySelector('.link-programms');
 const linkDownload = document.querySelector('.link-download');
-const links = document.querySelectorAll('.menu__link');
+// const links = document.querySelectorAll('.menu__link');
 const windowsBtn = document.querySelector('.windows');
 const macosBtn = document.querySelector('.macos');
-// console.log(linkGames);
+const cardLinks = document.querySelectorAll('.card__link');
+
+console.log(downloadError);
 
 document.addEventListener('scroll', function () {
-  // console.log(window.pageYOffset);
+  console.log(window.pageYOffset);
   if (window.pageYOffset >= 769) {
     header.classList.add('visible');
   } else {
@@ -24,13 +28,13 @@ document.addEventListener('scroll', function () {
     linkGames.classList.add('link-active');
   }
 
-  if (window.pageYOffset >= 1200 && window.pageYOffset <= 2500) {
+  if (window.pageYOffset >= 1200 && window.pageYOffset <= 2400) {
     linkGames.classList.remove('link-active');
     linkDownload.classList.remove('link-active');
     linkProgramms.classList.add('link-active');
   }
 
-  if (window.pageYOffset >= 2501) {
+  if (window.pageYOffset >= 2401) {
     linkGames.classList.remove('link-active');
     linkProgramms.classList.remove('link-active');
     linkDownload.classList.add('link-active');
@@ -53,6 +57,7 @@ document.addEventListener('scroll', function () {
 
   menuCloseItem.addEventListener('click', () => {
     menu.classList.remove('navigation_active');
+    document.body.style.overflow = 'visible';
   });
 
   for (const link of links) {
@@ -68,15 +73,32 @@ document.addEventListener('scroll', function () {
     macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
     windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
 
-  console.log(platform);
+  // console.log(platform);
   if (windowsPlatforms.indexOf(platform) !== -1) {
     windowsBtn.classList.remove('none');
     macosBtn.classList.add('none');
   } else if (macosPlatforms.indexOf(platform) !== -1) {
     macosBtn.classList.remove('none');
     windowsBtn.classList.add('none');
-  } else if (!windowsPlatforms && !macosPlatforms) {
-    return 'No download available for your OS';
+  } else if (!windowsPlatforms && !macosPlatforms && /Linux/.test(platform)) {
+    downloadMain.classList.add('none');
+    download.append(downloadError);
   }
   return;
+})();
+
+(function () {
+  cardLinks.forEach((link) => {
+    link.onauxclick = function (e) {
+      e.preventDefault();
+    };
+  });
+})();
+
+(function () {
+  cardLinks.forEach((link) => {
+    link.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+    });
+  });
 })();
